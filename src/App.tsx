87f3d8452e3dd0b0aed7cd510dceefa01cbba75e
Luca Pagliaro <pagliaroluca@gmail.com>
2022-03-19 21:41:51 +0100

    feat: added credits and disclaimer

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,14 @@ function App() {
   return (
       <div className="App">
         <div className="container">
+          <h1>Ikea card system with dots</h1>
+          <p>Prototype made by <a href="https://www.sentiero.digital/" target={'_blank'}>Sentiero.digital</a>, check the
+            code on <a href="https://github.com/Sentiero-digital/ikea-card-system"
+            target="_blank" rel="noreferrer">Github</a></p>
+          <p><small>
+            This project is a <abbr title="Proof of concept">PoC</abbr>, and may need improvements for production enviroment.
+          </small></p>
+          <hr/>
           <IkeaGrid/>
         </div>
       </div>
